f0757ea4395d2a829a123b04606fb244d851946b
José Tanicuchí <josluistanic99@outlook.es>
2023-09-23 20:38:32 -0500

    Tenant dashboard and welcome pages added

--- routes/tenant.php
+++ routes/tenant.php
@@ -24,7 +24,12 @@ Route::middleware([
     PreventAccessFromCentralDomains::class,
 ])->group(function () {
     Route::get('/', function () {
-        dd(\App\Models\User::all());
-        return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
+        return view('tenant.welcome');
     });
+
+    Route::get('/dashboard', function () {
+        return view('tenant.dashboard');
+    });
+
+    require __DIR__ . '/auth.php';
 });
